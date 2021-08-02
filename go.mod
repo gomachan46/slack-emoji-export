69ba5f18c49ec950a6a7e07f3725b8bcc272e544
gomachan46 <shiro.gomachan46@gmail.com>
2021-08-03 04:37:09 +0900

    add export slack emoji

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/gomachan46/slack_emoji
+
+go 1.16
+
+require (
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/slack-go/slack v0.9.4
+)
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/slack-go/slack"
+)
+
+var (
+	help = flag.Bool("help", false, "show help")
+)
+
+type config struct {
+	Token string `envconfig:"SLACK_USER_OAUTH_TOKEN" required:"true"`
+}
+
+func main() {
+	flag.Parse()
+	if *help {
+		flag.Usage()
+		return
+	}
+
+	if err := runMain(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func runMain() error {
+	var cfg config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return fmt.Errorf("failed to process envconfig: %w", err)
+	}
+
+	c := slack.New(cfg.Token)
+	emojis, err := c.GetEmoji()
+	if err != nil {
+		return err
+	}
+
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		return err
+	}
+
+	for emoji, url := range emojis {
+		if strings.HasPrefix(url, "alias:") {
+			continue
+		}
+
+		if err := download(url, filepath.Join(tmpDir, fmt.Sprintf("%s.png", emoji))); err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("Output directory: %s\n", tmpDir)
+	return nil
+}
+
+func download(url, filepath string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	file, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	io.Copy(file, response.Body)
+
+	return nil
+}
